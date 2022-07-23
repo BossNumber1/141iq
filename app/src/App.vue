71feae225f08d3f8fb24a6b0b45906375901bf97
BossNumber1 <dimaang23@gmail.com>
2022-07-23 14:24:32 +0300

    Made the first version of the app

---
+++ app/src/App.vue
@@ -0,0 +1,87 @@
+<template>
+  <div class="container">
+    <div class="header">
+      <div class="header__title">Фильтрация списка на Vue</div>
+      <div class="header__subtitle">Делаю сложное простым</div>
+    </div>
+    <div class="main">
+      <div class="main__demo">
+        <div class="titleBlock">Что получаем</div>
+        <div class="main__demo-content">
+          <FilterContacts />
+        </div>
+      </div>
+      <div class="main__src">
+        <div class="titleBlock">Как достигаем</div>
+        <div class="main__src-content">
+          <img src="https://clck.ru/sMkBL" alt="src guide" />
+        </div>
+      </div>
+    </div>
+    <div class="footer"></div>
+  </div>
+</template>
+
+<script>
+import FilterContacts from "./components/FilterContacts";
+
+export default {
+  name: "App",
+  components: {
+    FilterContacts,
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+@import url("https://fonts.googleapis.com/css2?family=Roboto+Slab:wght@100&display=swap");
+
+body {
+  font-family: "Roboto Slab", serif;
+}
+
+.header {
+  padding-top: 50px;
+  margin-bottom: 120px;
+
+  &__title {
+    font-size: 28px;
+    font-weight: bold;
+    text-align: center;
+    margin-bottom: 10px;
+  }
+
+  &__subtitle {
+    color: gray;
+    font-size: 14px;
+    text-align: center;
+  }
+}
+
+.main {
+  &__demo {
+    &-content {
+      margin-top: 50px;
+      // border: 1px solid;
+    }
+  }
+
+  &__src {
+    &-content {}
+  }
+}
+
+.footer {}
+
+.titleBlock {
+  text-align: center;
+  font-size: 20px;
+  margin: 10px;
+}
+
+img {
+  width: 100%;
+  height: auto;
+}
+</style>
+
---
+++ app/src/components/FilterContacts.vue
@@ -0,0 +1,200 @@
+<template>
+    <div class="container">
+        <div class="header">
+            <div class="header__inputArea">
+                <div class="header__inputIcon">
+                    <img src="https://clck.ru/sMgeU" alt="search icon" />
+                </div>
+                <div class="header__inputContent">
+                    <input type="text" placeholder="Введите данные для поиска"
+                        @input="(event) => searchInList(event)" />
+                </div>
+            </div>
+        </div>
+        <div class="main">
+            <div class="usersList">
+                <div class="list" v-if="!emptyArrayUsers">
+                    <div class="userInfo" v-for="user in users" :key="user.id">
+                        <div class="userAvatar">
+                            <img :src="user.avatar" :alt="user.alt" />
+                        </div>
+                        <div class="userData">
+                            <div class="userName">
+                                {{ user.name }}
+                            </div>
+                            <div class="userResidence">
+                                {{ user.residence }}
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="main__warning" v-else>
+                    <div class="main__warning-icon">
+                        <img src="https://clck.ru/sMhds" alt="attention icon" />
+                    </div>
+                    <div class="main__warning-content">
+                        Такого пользователя нет в Базе Данных
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+const sourceArrayUsers = [
+    {
+        id: 1,
+        avatar: "./pictures/avatars/Pheophan.png",
+        alt: "Pheophan avatar",
+        name: "Feofan",
+        residence: "Moscow",
+    },
+    {
+        id: 2,
+        avatar: "./pictures/avatars/Anna.png",
+        alt: "Anna avatar",
+        name: "Анна Краснова",
+        residence: "Saint-Petersburg",
+    },
+    {
+        id: 3,
+        avatar: "./pictures/avatars/Evgeny.png",
+        alt: "Evgeny avatar",
+        name: "Евгений Анисимов",
+        residence: "Veliky Novgorod",
+    },
+];
+
+export default {
+    name: "FilterContacts",
+    data() {
+        return {
+            users: sourceArrayUsers,
+            emptyArrayUsers: false,
+            contentInput: "",
+        };
+    },
+    methods: {
+        searchInList(event) {
+            const search = require("search-in-list"); // включаем библиотеку поиска
+
+            if (event.target.value === "") {
+                // при очистке поля - возвращаем исходный список
+                this.users = sourceArrayUsers;
+            } else if (event.target.value.length > this.contentInput.length) {
+                this.users = search(this.users, event.target.value); // фильтруем исходный массив
+            } else if (event.target.value.length < this.contentInput.length) {
+                this.emptyArrayUsers = false;
+                this.users = search(sourceArrayUsers, event.target.value); // фильтруем исходный массив
+            } else {
+                this.users = search(sourceArrayUsers, event.target.value); // фильтруем исходный массив
+            }
+
+            this.contentInput = event.target.value; // сохраняем текущее содержимое
+
+            if (this.users.length === 0) {
+                // даём сигнал, что список доступных пользователей закончился
+                this.emptyArrayUsers = true;
+            }
+        },
+    },
+};
+</script>
+
+<style scoped lang="scss">
+.container {
+    padding: 0 20px;
+
+    .header {
+        padding: 6px 0;
+
+        &__inputArea {
+            border: 1px solid gray;
+            border-radius: 12px;
+            display: flex;
+            align-items: center;
+            padding: 0px;
+            width: 335px;
+            height: 56px;
+            align-self: stretch;
+            flex-grow: 0;
+
+            .header__inputContent {
+                // border: 1px solid orange;
+                display: flex;
+                flex-direction: column;
+                justify-content: center;
+                align-items: flex-start;
+                padding: 12px 12px 12px 0px;
+                width: 283px;
+                height: 42px;
+                flex: none;
+                order: 1;
+                flex-grow: 1;
+
+                input {
+                    width: 95%;
+                    border: none;
+                    outline: none;
+                }
+            }
+
+            margin-bottom: 22px;
+        }
+    }
+
+    .main {
+        .usersList {
+            margin-bottom: 24px;
+
+            .list {
+                .userInfo {
+                    display: flex;
+                    align-items: center;
+                    margin: 5px 0;
+
+                    .userAvatar {
+                        margin-right: 16px;
+                    }
+
+                    .userData {
+                        .userName {
+                            width: 282px;
+                            height: 18px;
+                            font-family: "Roboto";
+                            font-style: normal;
+                            font-weight: 400;
+                            font-size: 14px;
+                            line-height: 18px;
+                            color: #263238;
+                        }
+
+                        .userResidence {
+                            width: 282px;
+                            height: 18px;
+                            font-family: "Roboto";
+                            font-style: normal;
+                            font-weight: 400;
+                            font-size: 14px;
+                            line-height: 18px;
+                            display: flex;
+                            align-items: center;
+                            color: #9E9E9E;
+                        }
+                    }
+                }
+            }
+        }
+
+        &__warning {
+            display: flex;
+            align-items: center;
+
+            &-icon {
+                margin-right: 10px;
+            }
+        }
+    }
+}
+</style>

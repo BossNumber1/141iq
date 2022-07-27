010da5be49e86fb840fe41322efc52441d0632cd
BossNumber1 <dimaang23@gmail.com>
2022-07-27 20:29:27 +0300

    replaced images with text and improved the appearance

--- app/src/App.vue
+++ app/src/App.vue
@@ -1,8 +1,14 @@
 <template>
   <div class="container">
     <div class="header">
-      <div class="header__title display-1">Фильтрация списка на Vue</div>
-      <div class="header__subtitle lead">Делаю сложное простым</div>
+      <div class="title display-1"><mark>141</mark>iq</div>
+      <div class="header__subtitle lead text-muted">Делаю сложное простым</div>
+    </div>
+    <div class="placeForAds">
+
+    </div>
+    <div class="title display-1">
+      Фильтрация списка на Vue
     </div>
     <div class="main">
       <div class="main__demo">
@@ -24,9 +30,7 @@
                 <img src="../public/pictures/icons/hideCode.png" alt="icon code hide" />
               </div>
             </div>
-            <div class="code">
-              <img src="https://clck.ru/sNA3H" alt="a snapshot of the template" />
-            </div>
+            <div class="code" id="templateSrc"></div>
           </div>
           <div class="codeBlock">
             <div class="codeBlock-title badge text-bg-warning" @click="manageDisplayRequiredCode('1')">
@@ -38,9 +42,7 @@
                 <img src="../public/pictures/icons/hideCode.png" alt="icon code hide" />
               </div>
             </div>
-            <div class="code">
-              <img src="https://clck.ru/sNA5M" alt="a snapshot of the script" />
-            </div>
+            <div class="code" id="scriptSrc"></div>
           </div>
           <div class="codeBlock">
             <div class="codeBlock-title badge text-bg-warning" @click="manageDisplayRequiredCode('2')">
@@ -52,9 +54,7 @@
                 <img src="../public/pictures/icons/hideCode.png" alt="icon code hide" />
               </div>
             </div>
-            <div class="code">
-              <img src="https://clck.ru/sP43f" alt="a snapshot of the styles" />
-            </div>
+            <div class="code" id="stylesSrc"></div>
           </div>
         </div>
       </div>
@@ -66,6 +66,213 @@
 <script>
 import FilterContacts from "./components/FilterContacts";
 
+const db = [
+  {
+    id: 1,
+    src: `
+      <div class="container">
+         <div class="header">
+            <div class="header__inputArea">
+               <div class="header__inputIcon">
+                  <img src="https://clck.ru/sMgeU" alt="search icon" />
+               </div>
+               <div class="header__inputContent">
+                  <input type="text" placeholder="Введите данные для поиска" @input="(event) => searchInList(event)" />
+               </div>
+            </div>
+         </div>
+         <div class="main">
+            <div class="usersList">
+               <div class="list" v-if="!emptyArrayUsers">
+                  <div class="userInfo" v-for="user in users" :key="user.id">
+                     <div class="userAvatar">
+                        <img :src="user.avatar" :alt="user.alt" />
+                     </div>
+                     <div class="userData">
+                        <div class="userName">
+                           {{ user.name }}
+                        </div>
+                        <div class="userResidence">
+                           {{ user.residence }}
+                        </div>
+                     </div>
+                  </div>
+               </div>
+               <div class="main__warning" v-else>
+                  <div class="main__warning-icon">
+                     <img src="https://clck.ru/sMhds" alt="attention icon" />
+                  </div>
+                  <div class="main__warning-content">
+                     Такого пользователя нет в Базе Данных
+                  </div>
+               </div>
+            </div>
+         </div>
+      </div>`
+  },
+  {
+    id: 2,
+    src: `
+      const sourceArrayUsers = [
+         {
+            id: 1,
+            avatar: "./pictures/avatars/Pheophan.png",
+            alt: "Pheophan avatar",
+            name: "Feofan",
+            residence: "Moscow",
+         },
+         {
+            id: 2,
+            avatar: "./pictures/avatars/Anna.png",
+            alt: "Anna avatar",
+            name: "Анна Краснова",
+            residence: "Saint-Petersburg",
+         },
+         {
+            id: 3,
+            avatar: "./pictures/avatars/Evgeny.png",
+            alt: "Evgeny avatar",
+            name: "Евгений Анисимов",
+            residence: "Veliky Novgorod",
+         },
+      ];
+
+      export default {
+         name: "FilterContacts",
+         data() {
+            return {
+               users: sourceArrayUsers,
+               emptyArrayUsers: false,
+               contentInput: "",
+            };
+         },
+         methods: {
+            searchInList(event) {
+               const search = require("search-in-list"); // включаем библиотеку поиска
+
+               if (event.target.value === "") {
+                  // при очистке поля - возвращаем исходный список
+                  this.users = sourceArrayUsers;
+               } else if (event.target.value.length > this.contentInput.length) {
+                  this.users = search(this.users, event.target.value); // фильтруем исходный массив
+               } else if (event.target.value.length < this.contentInput.length) {
+                  this.emptyArrayUsers = false;
+                  this.users = search(sourceArrayUsers, event.target.value); // фильтруем исходный массив
+               } else {
+                  this.users = search(sourceArrayUsers, event.target.value); // фильтруем исходный массив
+               }
+
+               this.contentInput = event.target.value; // сохраняем текущее содержимое
+
+               if (this.users.length === 0) {
+                  // даём сигнал, что список доступных пользователей закончился
+                  this.emptyArrayUsers = true;
+               }
+            },
+         },
+      };`
+  },
+  {
+    id: 3,
+    src: `
+      .container {
+         padding: 0 20px;
+
+         .header {
+            padding: 6px 0;
+
+            &__inputArea {
+               border: 1px solid gray;
+               border-radius: 12px;
+               display: flex;
+               align-items: center;
+               padding: 0px;
+               width: 335px;
+               height: 56px;
+               align-self: stretch;
+               flex-grow: 0;
+
+               .header__inputContent {
+                  // border: 1px solid orange;
+                  display: flex;
+                  flex-direction: column;
+                  justify-content: center;
+                  align-items: flex-start;
+                  padding: 12px 12px 12px 0px;
+                  width: 283px;
+                  height: 42px;
+                  flex: none;
+                  order: 1;
+                  flex-grow: 1;
+
+                  input {
+                     width: 95%;
+                     border: none;
+                     outline: none;
+                  }
+               }
+
+               margin-bottom: 22px;
+            }
+         }
+
+         .main {
+            .usersList {
+               margin-bottom: 24px;
+               margin-left: 10px;
+
+               .list {
+                  .userInfo {
+                     display: flex;
+                     align-items: center;
+                     margin: 5px 0;
+
+                     .userAvatar {
+                        margin-right: 16px;
+                     }
+
+                     .userData {
+                        .userName {
+                           width: 282px;
+                           height: 18px;
+                           font-family: "Roboto";
+                           font-style: normal;
+                           font-weight: 400;
+                           font-size: 14px;
+                           line-height: 18px;
+                           color: #263238;
+                        }
+
+                        .userResidence {
+                           width: 282px;
+                           height: 18px;
+                           font-family: "Roboto";
+                           font-style: normal;
+                           font-weight: 400;
+                           font-size: 14px;
+                           line-height: 18px;
+                           display: flex;
+                           align-items: center;
+                           color: #9E9E9E;
+                        }
+                     }
+                  }
+               }
+            }
+
+            &__warning {
+               display: flex;
+               align-items: center;
+
+               &-icon {
+                  margin-right: 10px;
+               }
+            }
+         }
+      }`
+    },
+]
+
 export default {
   name: "App",
   components: {
@@ -76,6 +283,7 @@ export default {
       showFirstPartCode: false,
       showSecondPartCode: false,
       showThirdPartCode: false,
+      src: db
     };
   },
   methods: {
@@ -124,6 +332,11 @@ export default {
       }
     },
   },
+  mounted() {
+    document.querySelector("#templateSrc").innerText = this.src[0].src;
+    document.querySelector("#scriptSrc").innerText = this.src[1].src;
+    document.querySelector("#stylesSrc").innerText = this.src[2].src;
+  }
 };
 </script>
 
@@ -137,20 +350,20 @@ body {
 
 .header {
   padding-top: 50px;
-  margin-bottom: 120px;
-
-  &__title {
-    // font-size: 28px;
-    // font-weight: bold;
-    text-align: center;
-    margin-bottom: 10px;
-  }
+  margin-bottom: 30px;
 
   &__subtitle {
     // color: gray;
     // font-size: 14px;
     text-align: center;
   }
+}
+
+.placeForAds {
+  width: 100%;
+  border: 1px solid;
+  height: 280px;
+  margin: 50px 0;
 }
 
 .main {
@@ -164,6 +377,8 @@ body {
   &__src {
     &-content {
       .codeBlock {
+        margin-bottom: 10px;
+
         &-title {
           background-color: lightgray;
           // color: #d61a1a;
@@ -189,6 +404,11 @@ body {
 }
 
 .footer {}
+
+.title {
+    text-align: center;
+    margin-bottom: 30px;
+}
 
 .titleBlock {
   text-align: center;

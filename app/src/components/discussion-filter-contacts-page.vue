3fd76e4a5fd47ee41a1480087745acb30dc70328
BossNumber1 <dimaang23@gmail.com>
2022-08-06 23:03:32 +0300

    Added discussion

---
+++ app/src/components/discussion-filter-contacts-page.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="comments">
+    <vue-disqus shortname="your_shortname_disqus" url="https://141iq.ru"></vue-disqus>
+  </div>
+</template>
+
+<script>
+import Vue from 'vue'
+import VueDisqus from 'vue-disqus'
+
+export default {
+  name: "discussion-filter-contacts-page",
+  components: {VueDisqus},
+  mounted() {
+    Vue.use(VueDisqus)
+  }
+}
+</script>
+
+<style lang="scss">
+  
+</style>

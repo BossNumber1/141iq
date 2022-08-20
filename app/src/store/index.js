37b772f865ea0b0cdb291bd0461d9d48780cc7d5
BossNumber1 <dimaang23@gmail.com>
2022-08-20 19:20:58 +0300

    hmmm

--- app/src/store/index.js
+++ app/src/store/index.js
@@ -1,46 +1,46 @@
-// import Vue from 'vue'
-// import Vuex from 'vuex'
+import Vue from 'vue'
+import Vuex from 'vuex'
 
-// Vue.use(Vuex)
+Vue.use(Vuex)
 
-// export default new Vuex.Store({
-//   state: {
-//     userName: "",
-//     userAva: ""
-//   },
-//   getters: {
-//     getUserName(state) {
-//       return state.userName
-//     },
-//     getUserAva(state) {
-//       return state.userAva
-//     }
-//   },
-//   mutations: {
-//     saveUserName(state, name) {
-//       state.userName = name
-//     },
-//     saveUserAva(state, ava) {
-//       state.userAva = ava
-//     }
-//   },
-//   actions: {
-//   },
-//   modules: {
-//   }
-// })
-
-// import { createApp } from 'vue'
-import { createStore } from 'vuex'
-
-// Create a new store instance.
-const store = createStore({
-  state () {
-    return {
-      userName: "",
-      userAva: ""
+export default new Vuex.Store({
+  state: {
+    userName: "",
+    userAva: ""
+  },
+  getters: {
+    getUserName(state) {
+      return state.userName
+    },
+    getUserAva(state) {
+      return state.userAva
     }
   },
+  mutations: {
+    saveUserName(state, name) {
+      state.userName = name
+    },
+    saveUserAva(state, ava) {
+      state.userAva = ava
+    }
+  },
+  actions: {
+  },
+  modules: {
+  }
+})
+
+// import { createApp } from 'vue'
+// import { createStore } from 'vuex'
+
+// Create a new store instance.
+// const store = createStore({
+//   state () {
+//     return {
+//       userName: "",
+//       userAva: ""
+//     }
+//   },
   // getters: {
   //   getUserName(state) {
   //     return state.userName
@@ -49,20 +49,20 @@ const store = createStore({
   //     return state.userAva
   //   }
   // },
-  mutations: {
-    saveUserName(state, name) {
-      state.userName = name
-    },
-    saveUserAva(state, ava) {
-      state.userAva = ava
-    }
-  }
+//   mutations: {
+//     saveUserName(state, name) {
+//       state.userName = name
+//     },
+//     saveUserAva(state, ava) {
+//       state.userAva = ava
+//     }
+//   }
 
-})
+// })
 
 // const app = createApp({<App /> })
 
 // Install the store instance as a plugin
 // app.use(store)
 
-export default store
+// export default store
